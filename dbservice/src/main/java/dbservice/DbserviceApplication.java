91c0d5c21de186881c6c254401bd0130e789a3fe
Sumit Prakash <sumit.prak@gmail.com>
2018-06-25 14:40:49 +1000

    Added eureka service registry, registered services with eureka

--- dbservice/src/main/java/dbservice/DbserviceApplication.java
+++ dbservice/src/main/java/dbservice/DbserviceApplication.java
@@ -2,9 +2,11 @@ package dbservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+@EnableEurekaClient //Spring framework takes care of discovery through this
 @EnableTransactionManagement
 @EnableJpaRepositories(basePackages = {"dbservice.repositories"})
 @SpringBootApplication
--- fund_transfer_service/src/main/java/fundtransfer/FundTransferServiceApplication.java
+++ fund_transfer_service/src/main/java/fundtransfer/FundTransferServiceApplication.java
@@ -2,11 +2,13 @@ package fundtransfer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.client.RestTemplate;
 
+@EnableEurekaClient //Spring framework takes care of discovery through this
 @EnableTransactionManagement
 @EnableJpaRepositories(basePackages = {"fundtransfer.repositories"})
 @SpringBootApplication
